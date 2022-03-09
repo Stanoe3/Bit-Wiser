648c0fc9ab5d2f380e3bc55ff17f4f54ef94bd57
Stanoe3 <stanbrown1605@gmail.com>
2022-03-09 16:44:30 +0000

    Basic creation

---
+++ bitwiser/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>MERN-Test</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
