81f2e35b63757f5d2426d77cb98f77668ac139ba
Stanoe3 <stanbrown1605@gmail.com>
2022-03-11 12:48:53 +0000

    Changes to some action files

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,17 @@
-import React from 'react';
 import Navitem from "./Navitem";
 
 const Navbar = ({ items }) => {
-    let list = []
-    for (let i = 0; i < items.length; i++) {
-        list.push(<Navitem key={i} item={items[i]}/>)
-    }
-    return (
-        <div className="navbar">
-            <nav>
-                <ul>
-                    { list }
-                </ul>
-            </nav>
-            
-        </div>
-    )
-}
+	let list = [];
+	for (let i = 0; i < items.length; i++) {
+		list.push(<Navitem key={i} item={items[i]} />);
+	}
+	return (
+		<div className="navbar">
+			<nav>
+				<ul>{list}</ul>
+			</nav>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
